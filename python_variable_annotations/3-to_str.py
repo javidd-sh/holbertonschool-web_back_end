f40f8503c628cf9d8a706ef95e1e3b101ad6d2eb
Your Name <you@example.com>
2026-04-29 11:34:49 +0000

    salam

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module providing a to_str function.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a given float.
+
+    Args:
+        n (float): The number to convert to a string.
+
+    Returns:
+        str: The string representation of n.
+    """
+    return str(n)
