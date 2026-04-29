d2692e51ffa53c327fd2b6b3e0a7aad4af39f0c8
Your Name <you@example.com>
2026-04-29 11:38:46 +0000

    salam

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -14,7 +14,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         v (Union[int, float]): The number to be squared.
 
     Returns:
-        Tuple[str, float]: A tuple where the first element is k 
+        Tuple[str, float]: A tuple where the first element is k
         and the second is the square of v (as a float).
     """
     return (k, float(v**2))
