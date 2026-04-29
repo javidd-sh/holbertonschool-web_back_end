1492c3ef3f6c30643b9a006394ee07ab5142ebe0
Your Name <you@example.com>
2026-04-29 11:33:47 +0000

    salam

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module providing a floor function.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a given float.
+
+    Args:
+        n (float): The number to be floored.
+
+    Returns:
+        int: The floor of n.
+    """
+    return math.floor(n)
