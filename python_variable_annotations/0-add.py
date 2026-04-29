3504cb44bcf1d2f72a04800580feb3efafe20276
Your Name <you@example.com>
2026-04-29 11:28:54 +0000

    salam

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the result.
+    
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+        
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
