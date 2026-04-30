74d85c523d0f95d0c70829044eb334c2d58b79ec
Your Name <you@example.com>
2026-04-30 15:00:09 +0000

    salam

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """This module provides an async function that waits for a random delay."""
+
 import asyncio
 import random
+
+
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay between 0 and max_delay and return it."""
     delay = random.uniform(0, max_delay)
