14da2f8e466065d890d712839e1d347027a9269e
Your Name <you@example.com>
2026-04-30 15:14:12 +0000

    salam

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
-Async Comprehension module
+Import async_generator from the previous task and then write a coroutine 
+called async_comprehension that takes no arguments.
 """
+import asyncio
 from typing import List
 
-# Importing the generator from the previous task
+# Dynamic import as requested by the task style
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """
-    Collect 10 random numbers using an async comprehension 
-    over async_generator, then return the 10 random numbers.
+    Collect 10 random numbers using an async comprehension
     """
     return [i async for i in async_generator()]
