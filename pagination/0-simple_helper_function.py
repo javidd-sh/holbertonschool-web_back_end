a60260069f9477520faa230cecfda1b6bb2040c5
Your Name <you@example.com>
2026-07-15 17:26:34 +0000

    salam

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-page
+Module containing a simple helper function for pagination.
 """
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    page
+    Calculate the start and end indexes for a given page and page size.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
