269981f123c06e1524cba8943725de8b90ea8268
Your Name <you@example.com>
2026-08-05 15:58:41 +0000

    salam

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import router from './routes/index';
+import controllerRouting from './routes/index';
 
 const app = express();
 const port = 1245;
 
-app.use('/', router);
+controllerRouting(app);
 
-app.listen(port);
+app.listen(port, () => {
+  //   console.log(`Example app listening at http://localhost:${port}`);
+});
 
 export default app;
